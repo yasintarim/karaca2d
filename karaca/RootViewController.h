9211785e9d41430d9a3d43f67da161be2c6696a4
Yasin Tarım <yasintar@gmail.com>
2011-09-07 22:45:26 +0300

    Initial Commit

---
+++ karaca/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  karaca
+//
+//  Created by yasin on 9/7/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
